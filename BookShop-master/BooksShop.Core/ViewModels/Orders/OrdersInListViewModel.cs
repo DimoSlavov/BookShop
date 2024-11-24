e5ef4dbf1b4335fb2c1a775625ccb28646e61aa1
DimoSlavov <dimo_slavov87@abv.bg>
2024-11-24 16:51:42 +0200

    upgrade project

---
+++ BookShop-master/BooksShop.Core/ViewModels/Orders/OrdersInListViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BooksShop.Core.ViewModels.Orders
+{
+    public class OrdersInListViewModel : OrderBaseViewModel
+    {
+        public IEnumerable<BookOrderViewModel> BookOrders { get; set; } =
+            new List<BookOrderViewModel>();
+
+        public int ItemsCount => BookOrders.Select(x => x.Quantity).Sum();
+
+        public decimal BooksTotalSum => BookOrders.Select(x => x.BookTotalPrice).Sum() + ShippingFee;
+    }
+}
