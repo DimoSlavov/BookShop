25bc64a1463f35a639c33c94b1d548f310b3ec29
DimoSlavov <dimo_slavov87@abv.bg>
2024-11-24 19:19:48 +0200

    upgrade project

---
+++ BookShop-master/BooksShop.Infrastructure/Data/Message.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="Message.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace BooksShop.Infrastructure.Data
+{
+    using System.ComponentModel.DataAnnotations;
+    using static BooksShop.Infrastructure.Data.Constants;
+
+    public class Message
+    {
+        public int Id { get; set; }
+
+        [MaxLength(FirstNameMaxLength)]
+        public string FirstName { get; set; } = null!;
+
+        [MaxLength(LastNameMaxLength)]
+        public string LastName { get; set; } = null!;
+
+        [MaxLength(EmailMaxLenght)]
+
+        public string Email { get; set; } = null!;
+
+        [MaxLength(PhoneNumberMaxLenght)]
+        public string? Phone { get; set; }
+
+        [MaxLength(SubjectMaxLength)]
+        public string Subject { get; set; }
+
+        [MaxLength(MessageContentMaxLenght)]
+        public string Content { get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+    }
+}
