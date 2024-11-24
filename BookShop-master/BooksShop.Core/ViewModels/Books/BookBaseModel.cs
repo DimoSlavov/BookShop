e19fd7ce377fb1a14da28c4aaa1810130fd4f078
DimoSlavov <dimo_slavov87@abv.bg>
2024-11-24 15:38:21 +0200

    upgrade project

---
+++ BookShop-master/BooksShop.Core/ViewModels/Books/BookBaseModel.cs
@@ -0,0 +1,40 @@
+﻿namespace BooksShop.Core.ViewModels.Books
+{
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using static BooksShop.Infrastructure.Data.Constants;
+
+    public class BookBaseModel
+    {
+        [Required(ErrorMessage = TitleErrorMessage)]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = AuthorErrorMessage)]
+        [StringLength(AuthorMaxLength, MinimumLength = AuthorMinLength)]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = ISBNErrorMessage)]
+        [StringLength(ISBNMaxLength, MinimumLength = ISBNMinLength)]
+
+        public string ISBN { get; set; }
+
+        [Required(ErrorMessage = NumOfPagesErrorMessage)]
+        [Range(MinNumOfPages, MaxNumOfPages, ErrorMessage = NumOfPagesRangeErrorMessage)]
+
+        public int NumOfPages { get; set; }
+
+        [Required(ErrorMessage = PriceErrorMessage)]
+
+        public decimal Price { get; set; }
+
+        [Required]
+
+        public int CategoryId { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; } = new List<SelectListItem>();
+
+        [MaxLength(DescriptionMaxLength, ErrorMessage = MaxLengthDescriptionErrorMessage)]
+        public string? Description { get; set; } = null;
+    }
+}
---
+++ BookShop-master/BooksShop.Core/ViewModels/Books/BookIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BooksShop.Core.ViewModels.Books
+{
+    public class BookIndexViewModel
+    {
+        public IEnumerable<BookViewModel> NewestBooks { get; set; }
+
+        public IEnumerable<BookViewModel> TopSalesBooks { get; set; }
+
+        public string Search { get; set; }
+    }
+}
