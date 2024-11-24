c2ea4ec9011c28d02cd47c2b0abd2d3dc03affdb
DimoSlavov <dimo_slavov87@abv.bg>
2024-11-24 17:37:25 +0200

    upgrade project

---
+++ BookShop-master/BooksShop.Core/ViewModels/ShoppingCart/BookInOrderViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BooksShop.Core.ViewModels.ShoppingCart
+{
+    public class BookInOrderViewModel : BookBaseViewModel
+    {
+        public int Quantity { get; set; }
+
+        public decimal TotalPrice => this.Price * this.Quantity;
+    }
+}
---
+++ BookShop-master/BooksShop.Core/ViewModels/ShoppingCart/OrderModel.cs
@@ -0,0 +1,21 @@
+﻿namespace BooksShop.Core.ViewModels.ShoppingCart
+{
+    using BooksShop.Infrastructure.Data.Enums;
+
+    public class OrderModel
+    {
+        public IEnumerable<BookInOrderViewModel> OrderedBooks { get; set; } = Enumerable.Empty<BookInOrderViewModel>();
+
+        public string CookieValue { get; set; }
+
+        public decimal Subtotal { get; set; }
+
+        public string? DeliveryAddress { get; set; }
+
+        public PaymentMethod? PaymentMethod { get; set; }
+
+        public decimal ShippingFee => 5;
+
+        public decimal Total => this.Subtotal + this.ShippingFee;
+    }
+}
