7cec1a43f0de185da3acdeda1e9f1bc0702fc6f5
DimoSlavov <dimo_slavov87@abv.bg>
2024-11-24 18:18:20 +0200

    upgrade project

---
+++ BookShop-master/BooksShop.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿namespace BooksShop.Data
+{
+    using BooksShop.Infrastructure.Data;
+    using BooksShop.Infrastructure.Data.Configuration;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<BookOrder> BooksOrders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new BookConfiguration());
+            builder.ApplyConfiguration(new RoleConfiguration());
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new UserRoleConfiguration());
+            builder.ApplyConfiguration(new OrderConfiguration());
+            builder.Entity<BookOrder>()
+                .HasKey(x => new { x.BookId, x.OrderId });
+            builder.ApplyConfiguration(new BookOrderConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
