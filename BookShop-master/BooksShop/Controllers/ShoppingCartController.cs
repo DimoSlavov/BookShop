b1c148806f59f5be642ff2c5308c619c385e0e2c
DimoSlavov <dimo_slavov87@abv.bg>
2024-11-28 11:59:44 +0200

    upgrade project

---
+++ BookShop-master/BooksShop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,106 @@
+﻿namespace BooksShop.Controllers
+{
+    using System;
+    using System.Security.Claims;
+    using BooksShop.Core.Contracts;
+    using BooksShop.Core.ViewModels.ShoppingCart;
+    using BooksShop.Infrastructure.Data;
+    using BooksShop.Infrastructure.Data.Enums;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using static BooksShop.Infrastructure.Data.Constants;
+
+    public class ShoppingCartController : Controller
+    {
+        private readonly IShoppingCartService shoppingCartService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public ShoppingCartController(
+            IShoppingCartService shoppingCartService,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.shoppingCartService = shoppingCartService;
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Cart(
+            string? act,
+            int bookId,
+            string? deliveryAddress,
+            PaymentMethod? paymentMethod)
+        {
+            string cookieValue = this.HttpContext.Request.Cookies[ShoppingCart] ?? string.Empty;
+
+            string? userName = this.User.Identity?.Name;
+            if (userName != null)
+            {
+                ApplicationUser currentUser = await this.userManager.FindByNameAsync(userName);
+                deliveryAddress = currentUser.Address;
+            }
+
+            OrderModel model = await this.shoppingCartService
+                .ShoppingCartInfo(cookieValue, act, bookId, deliveryAddress, paymentMethod);
+
+            CookieOptions cookieOptions = new CookieOptions();
+            cookieOptions.Expires = DateTime.Now.AddDays(365);
+            cookieOptions.Path = "/";
+
+            this.Response.Cookies.Append(ShoppingCart, model.CookieValue, cookieOptions);
+
+            if (act != null && bookId != 0 && model.CookieValue != cookieValue)
+            {
+                // Redirect to the same page:
+                //   - to remove the query string from the url
+                //   - to set the shopping cart size using the updated cookie
+                this.Response.Redirect(this.Request.Path.ToString());
+            }
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Cart(OrderModel model)
+        {
+            if (model.DeliveryAddress == null || model.PaymentMethod == null)
+            {
+                this.TempData[Constants.Message] = OrderDataValidation;
+                return this.View(model);
+            }
+
+            try
+            {
+                string? userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+                if (userId == null)
+                {
+                    return this.RedirectToAction("Login", "User");
+                }
+
+                string cookieValue = this.HttpContext.Request.Cookies[ShoppingCart] ?? string.Empty;
+
+                if (string.IsNullOrEmpty(cookieValue))
+                {
+                    this.TempData[Constants.Message] = ShoppingCartEmpty;
+                    return this.View(model);
+                }
+
+                model.CookieValue = cookieValue;
+                await this.shoppingCartService.CreateOrder(model, userId);
+
+                // Delete the Cookie "shopping_cart" from browser
+                this.Response.Cookies.Delete(ShoppingCart);
+
+                this.TempData[Constants.Message] = OrderCreated;
+                return this.RedirectToAction(nameof(this.Cart));
+            }
+            catch (Exception ex)
+            {
+                this.ModelState.AddModelError(string.Empty, ex.Message);
+                this.TempData[Constants.Message] = ex.Message;
+                return this.View(model);
+            }
+        }
+    }
+}
---
+++ BookShop-master/BooksShop/Controllers/UserController.cs
@@ -0,0 +1,163 @@
+﻿namespace BooksShop.Controllers
+{
+    using BooksShop.Core.Contracts;
+    using BooksShop.Core.ViewModels;
+    using BooksShop.Core.ViewModels.Users;
+    using BooksShop.Infrastructure.Data;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using static BooksShop.Infrastructure.Data.Constants;
+    using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly IUserService userService;
+
+        public UserController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IUserService userService)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            RegisterViewModel model = new RegisterViewModel();
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            ApplicationUser newUser = new ApplicationUser()
+            {
+                 UserName = model.UserName,
+                 FirstName = model.Firstname,
+                 LastName = model.Lastname,
+                 Email = model.Email,
+                 PhoneNumber = model.PhoneNumber,
+                 Address = model.Address,
+                 RegisteredOn = DateTime.Now,
+            };
+
+            IdentityResult result = await this.userManager.CreateAsync(newUser, model.Password);
+            result = await this.userManager.AddToRoleAsync(newUser, ClientRoleName);
+
+            if (result.Succeeded)
+            {
+                return this.RedirectToAction(nameof(this.Login));
+            }
+
+            foreach (IdentityError error in result.Errors)
+            {
+                this.ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return this.View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginViewModel model = new LoginViewModel();
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            ApplicationUser user = await this.userManager.FindByNameAsync(model.UserName);
+
+            if (user != null)
+            {
+                SignInResult result = await this.signInManager.PasswordSignInAsync(
+                    user, model.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return this.RedirectToAction("Index", "Home");
+                }
+            }
+
+            this.ModelState.AddModelError(string.Empty, SignInErrorMessage);
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await this.signInManager.SignOutAsync();
+            return this.RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Profile(string id)
+        {
+            ApplicationUser currentUser = await this.userManager.FindByIdAsync(id);
+
+            if (currentUser == null)
+            {
+                return this.NotFound();
+            }
+
+            UserEditModel model = await this.userService.GetUserProfile(id);
+
+            string? role = this.userManager.GetRolesAsync(currentUser).Result
+               .FirstOrDefault();
+
+            if (role != null)
+            {
+                model.Role = role;
+            }
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Profile(UserEditModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            ApplicationUser user = await this.userManager.FindByIdAsync(model.Id);
+
+            if (user == null)
+            {
+                return this.NotFound();
+            }
+
+            try
+            {
+                await this.userService.UpdateUserAsync(user, model);
+                IdentityResult result = await this.userManager.UpdateAsync(user);
+                this.TempData[Constants.Message] = UserUpdatedSuccessfully;
+                return this.RedirectToAction(nameof(this.Profile));
+            }
+            catch (Exception ex)
+            {
+                this.ModelState.AddModelError(string.Empty, ex.Message);
+                this.TempData[Constants.Message] = ex.Message;
+                return this.View(model);
+            }
+        }
+    }
+}
